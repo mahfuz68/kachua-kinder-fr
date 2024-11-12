c077e3cac50f136e6ed176253314fe5e43f7757e
mahfuz anam <mymsicret34@gmail.com>
2024-11-12 16:37:01 +0600

    limit session expire

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -16,37 +16,44 @@ const handler = NextAuth({
       },
 
       async authorize(credentials) {
-        // Add logic here to look up the user from the credentials supplied
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                mobile: credentials?.username,
+                password: credentials?.password,
+              }),
+            }
+          );
 
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              mobile: credentials?.username,
-              password: credentials?.password,
-            }),
+          if (res.status === 401) return null;
+          const user = await res.json();
+
+          if (user) {
+            return user;
+          } else {
+            // If you return null then an error will be displayed advising the user to check their details.
+            return null;
           }
-        );
-
-        if (res.status === 401) return null;
-        const user = await res.json();
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        } catch (e) {
+          throw new Error("Auth failed");
         }
       },
     }),
   ],
+
+  session: {
+    strategy: "jwt",
+    maxAge: 7 * 24 * 60 * 60, // 2 minutes in seconds
+  },
+  jwt: {
+    maxAge: 7 * 24 * 60 * 60, // 2 minutes in seconds
+  },
 
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
