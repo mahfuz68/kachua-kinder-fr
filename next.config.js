65755231a83dbb97df0fb3195fb974778f834848
Mahfuz Anam <94157542+mahfuz68@users.noreply.github.com>
2025-11-20 22:50:09 +0000

    add some change on next config file

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "api.kkabd.net",
+        hostname: "api.kkabd.org",
       },
       {
         protocol: "https",
